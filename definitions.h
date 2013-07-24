25e18a480f5bb857e4c918d9bc0dda06f4c14792
Spencer Gardner <spencergardner@gmail.com>
2013-07-24 06:59:51 -0500

    add definitions

---
+++ definitions.h
@@ -0,0 +1,13 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define col_pk_uid 0
+#define col_id_account 1
+#define col_relate_account 2
+#define col_date 3
+#define col_comment 4
+#define col_amount 5
+#define col_total 6
+#define col_reconciled 7
+
+#endif // DEFINITIONS_H
